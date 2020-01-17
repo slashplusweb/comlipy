94d86d4b97143af1b400e4d533cf1765fef749f3
slashplus <sp@slashplus.de>
2020-01-17 15:53:04 +0100

    Remove wrong files

--- build/lib/comlipy/lib/parser.py
+++
@@ -1,8 +0,0 @@
-
-
-
-class Parser:
-
-    def __init__(self):
-        print('hellasssssssaso world')
-
--- build/lib/comlipy/main.py
+++
@@ -1,8 +0,0 @@
-import click
-from .lib.parser import Parser
-
-
-@click.command()
-def cli():
-    click.echo('demo 1234 ')
-    Parser()
