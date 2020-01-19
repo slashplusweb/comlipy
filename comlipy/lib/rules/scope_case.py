cacb99f902cda53995268c1b64b7cddce9dd424f
slashplus <sp@slashplus.de>
2020-01-19 22:58:32 +0100

    Initial commit of scope_case rule module

---
+++ comlipy/lib/rules/scope_case.py
@@ -0,0 +1,22 @@
+from ..ensure import Ensure
+from .abstract_rule import AbstractRule
+
+
+class ScopeCase(AbstractRule):
+
+    def check(self):
+        scope = self._parser.scope
+
+        if scope is None:
+            return [True]
+
+        # convert to list if necessary
+        value = [self._value] if not isinstance(self._value, list) else self._value
+
+        return all(isinstance(case, str) and Ensure.is_case(scope, case) for case in value)
+
+    def execute(self):
+        result = self.check()
+        message = 'scope must {}be {}'.format('not ' if self.negated(self._when) else '', self._value), self._level
+
+        return result, message
