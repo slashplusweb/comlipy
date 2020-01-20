f06df8b755c5ece127eaf3a6c9e666cdb4e5b41b
slashplus <sp@slashplus.de>
2020-01-20 17:48:39 +0100

    Initial commit of type_max_length rule module

---
+++ comlipy/lib/rules/type_max_length.py
@@ -0,0 +1,19 @@
+from ..ensure import Ensure
+from .abstract_rule import AbstractRule
+
+
+class TypeMaxLength(AbstractRule):
+
+    def check(self):
+        type_string = self._parser.type
+
+        if type_string is None or not self.is_value_int():
+            return True
+
+        return Ensure.is_valid_length_max(type_string, self._value)
+
+    def execute(self):
+        result = self.check()
+        message = 'type must not be longer than {} characters'.format(self._value), self._level
+
+        return result, message
