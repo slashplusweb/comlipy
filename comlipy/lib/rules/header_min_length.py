f181436de60e667a6a0ff6c415f134c0211943dc
slashplus <sp@slashplus.de>
2020-01-20 17:47:46 +0100

    Initial commit of header_min_length rule module

---
+++ comlipy/lib/rules/header_min_length.py
@@ -0,0 +1,19 @@
+from ..ensure import Ensure
+from .abstract_rule import AbstractRule
+
+
+class HeaderMinLength(AbstractRule):
+
+    def check(self):
+        header = self._parser.header
+
+        if header is None or not self.is_value_int():
+            return True
+
+        return Ensure.is_valid_length_min(header, self._value)
+
+    def execute(self):
+        result = self.check()
+        message = 'header must not be shorter than {} characters'.format(self._value), self._level
+
+        return result, message
