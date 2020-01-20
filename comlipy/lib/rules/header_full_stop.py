deb1c539b5a648d59a0118846cdea6b6d6a02f73
slashplus <sp@slashplus.de>
2020-01-20 20:42:04 +0100

    Initial commit of header_full_stop rule module

---
+++ comlipy/lib/rules/header_full_stop.py
@@ -0,0 +1,20 @@
+from ..ensure import Ensure
+from .abstract_rule import AbstractRule
+
+
+class HeaderFullStop(AbstractRule):
+
+    def check(self):
+        header = self._parser.header
+
+        if header is None or not isinstance(self._value, str):
+            return True
+
+        return Ensure.is_last_character(header, self._value)
+
+    def execute(self):
+        result = self.check()
+        result = not result if self.negated(self._when) else result
+        message = 'header {} end with full stop'.format('may not' if self.negated(self._when) else 'must'), self._level
+
+        return result, message
