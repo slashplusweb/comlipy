672a6799b6b4303b52d17021de298944e1795416
slashplus <sp@slashplus.de>
2020-01-20 17:49:30 +0100

    Initial commit of type_case rule module

---
+++ comlipy/lib/rules/type_case.py
@@ -0,0 +1,22 @@
+from ..ensure import Ensure
+from .abstract_rule import AbstractRule
+
+
+class TypeCase(AbstractRule):
+
+    def check(self):
+        type_string = self._parser.type
+        if type_string is None:
+            return True
+
+        # convert to list if necessary
+        value = [self._value] if not isinstance(self._value, list) else self._value
+
+        return all(isinstance(case, str) and Ensure.is_case(type_string, case) for case in value)
+
+    def execute(self):
+        result = self.check()
+        result = not result if self.negated(self._when) else result
+        message = 'type must {}be {}'.format('not ' if self.negated(self._when) else '', self._value), self._level
+
+        return result, message
