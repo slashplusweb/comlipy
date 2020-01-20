a419f793d5a97a8099afbb1c01362350134af0e2
slashplus <sp@slashplus.de>
2020-01-20 17:49:15 +0100

    Initial commit of header_case rule module

---
+++ comlipy/lib/rules/header_case.py
@@ -0,0 +1,22 @@
+from ..ensure import Ensure
+from .abstract_rule import AbstractRule
+
+
+class HeaderCase(AbstractRule):
+
+    def check(self):
+        header = self._parser.header
+        if header is None:
+            return True
+
+        # convert to list if necessary
+        value = [self._value] if not isinstance(self._value, list) else self._value
+
+        return all(isinstance(case, str) and Ensure.is_case(header, case) for case in value)
+
+    def execute(self):
+        result = self.check()
+        result = not result if self.negated(self._when) else result
+        message = 'header must {}be {}'.format('not ' if self.negated(self._when) else '', self._value), self._level
+
+        return result, message
