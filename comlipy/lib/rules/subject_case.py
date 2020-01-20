9b8a34e3085c0d06c5474b4229025fca3d50e01c
slashplus <sp@slashplus.de>
2020-01-20 17:49:23 +0100

    Initial commit of subject_case rule module

---
+++ comlipy/lib/rules/subject_case.py
@@ -0,0 +1,22 @@
+from ..ensure import Ensure
+from .abstract_rule import AbstractRule
+
+
+class SubjectCase(AbstractRule):
+
+    def check(self):
+        subject = self._parser.subject
+        if subject is None:
+            return True
+
+        # convert to list if necessary
+        value = [self._value] if not isinstance(self._value, list) else self._value
+
+        return all(isinstance(case, str) and Ensure.is_case(subject, case) for case in value)
+
+    def execute(self):
+        result = self.check()
+        result = not result if self.negated(self._when) else result
+        message = 'subject must {}be {}'.format('not ' if self.negated(self._when) else '', self._value), self._level
+
+        return result, message
