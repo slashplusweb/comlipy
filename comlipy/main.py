3feb45a3d7fd5f85e155b34ce339e5f30aeb20a8
slashplus <sp@slashplus.de>
2020-01-20 15:58:12 +0100

    Change validations and messages mechanism

--- comlipy/main.py
+++ comlipy/main.py
@@ -1,10 +1,26 @@
+import sys
+
 import click
+from .lib.config import Config
 from .lib.parser import Parser
+from .lib.validator import Validator
+from .lib.messages import Messages
 
 
 @click.command()
 @click.argument('message')
-@click.option('-c', 'config_path', default='./', help='Path to the configuration file (.yml)')
-def cli(message, config_path):
-    click.echo(config_path)
-    Parser(message)
+@click.option('-c', '--config', 'config_file_path', default=None, help='path to the config file (.yml)')
+# @click.option('-q', '--quiet', 'is_quiet', default=None, help='Toggle console output')
+def cli(message, config_file_path):
+    # get the config
+    config = Config(config_file_path)
+    parser = Parser(message)
+
+    # init message system
+    messages = Messages(parser, config)
+
+    # validate all parts of the message in accordance with the given config
+    validator = Validator(config, parser, messages)
+    validator.validate()
+
+    messages.show()
