610e66a6f2d693139edd6d5375b9b08040f1f5dc
slashplus <sp@slashplus.de>
2020-01-20 15:56:30 +0100

    Remove re import dependency, Add Message instance, Change pass result of rules to Message

--- comlipy/lib/validator.py
+++ comlipy/lib/validator.py
@@ -2,14 +2,15 @@ from importlib import import_module
 from .config import Config
 from .parser import Parser
 from .ensure import Ensure
-from re import split
+from .messages import Messages
 
 
 class Validator:
 
-    def __init__(self, config: Config, parser: Parser):
+    def __init__(self, config: Config, parser: Parser, messages: Messages):
         self._config = config
         self._parser = parser
+        self._messages = messages
         self._ensure = Ensure()
 
     def validate(self):
@@ -24,7 +25,9 @@ class Validator:
             try:
                 rule_module = self.__get_rule_class(rule_key)
                 rule = rule_module(self._parser, self._config.get_rules_setting(rule_key))
-                rule.execute()
+                result, message = rule.execute()
+                if not result:
+                    self._messages.add_rule_result(message[0], message[1])
             except ModuleNotFoundError:
                 # todo: remove continue und raise exception
                 continue
@@ -37,7 +40,7 @@ class Validator:
 
     def __get_rule_class(selfs, rule: str):
         rule_module_name = rule.replace('-', '_')
-        rule_class_name = ''.join(part.capitalize() for part in split(r'-', rule))
+        rule_class_name = ''.join(part.capitalize() for part in rule.split('-'))
         rule_module = import_module('.{}'.format(rule_module_name), '{}.rules'.format(__package__))
 
         return getattr(rule_module, rule_class_name)
