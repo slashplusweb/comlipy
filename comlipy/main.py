6776bed4805a2e18f3b39b2367f1fdeca7063074
slashplus <sp@slashplus.de>
2020-01-20 22:50:53 +0100

    Add version flag, Add version contant, Clean code

--- comlipy/main.py
+++ comlipy/main.py
@@ -1,19 +1,22 @@
-import sys
-
 import click
 from .lib.config import Config
 from .lib.parser import Parser
 from .lib.validator import Validator
 from .lib.messages import Messages
 
+COMLIPY_VERSION = '1.0.0-rc'
+
 
 @click.command()
 @click.argument('message')
 @click.option('-c', '--config', 'config_file_path', default=None, help='path to the config file (.yml)')
 @click.option('-q', '--quiet', 'is_quiet', default=False, is_flag=True, help='Toggle console output')
+@click.version_option(version=COMLIPY_VERSION)
 def cli(message, config_file_path, is_quiet):
     # get the config
     config = Config(config_file_path)
+
+    # parse the commit message
     parser = Parser(message)
 
     # init message system
