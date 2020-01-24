f02d1900613d3817c0ed980813dd99a95a70ffc1
slashplus <sp@slashplus.de>
2020-01-24 17:01:35 +0100

    Change module name to plural, Add abstract subclasses

---
+++ comlipy/lib/rules/abstract_rules.py
@@ -0,0 +1,79 @@
+import abc
+
+from ..parser import Parser
+
+
+class AbstractRule(abc.ABC):
+
+    def __init__(self, parser: Parser, settings: list):
+        self._parser = parser
+        self._when, self._value, self._level = settings
+
+    def get_rule_input(self):
+        return getattr(self._parser, '_{}'.format(self.get_rule_type()))
+
+    @abc.abstractmethod
+    def get_rule_type(self):
+        pass
+
+    @abc.abstractmethod
+    def execute(self) -> (bool, str, int):
+        pass
+
+    @abc.abstractmethod
+    def check(self):
+        pass
+
+
+class AbstractHeaderRule(AbstractRule, abc.ABC):
+
+    def __init__(self, parser: Parser, settings: list):
+        super().__init__(parser, settings)
+
+    def get_rule_type(self):
+        return 'header'
+
+
+class AbstractBodyRule(AbstractRule, abc.ABC):
+
+    def __init__(self, parser: Parser, settings: list):
+        super().__init__(parser, settings)
+
+    def get_rule_type(self):
+        return 'body'
+
+
+class AbstractFooterRule(AbstractRule, abc.ABC):
+
+    def __init__(self, parser: Parser, settings: list):
+        super().__init__(parser, settings)
+
+    def get_rule_type(self):
+        return 'footer'
+
+
+class AbstractScopeRule(AbstractRule, abc.ABC):
+
+    def __init__(self, parser: Parser, settings: list):
+        super().__init__(parser, settings)
+
+    def get_rule_type(self):
+        return 'scope'
+
+
+class AbstractTypeRule(AbstractRule, abc.ABC):
+
+    def __init__(self, parser: Parser, settings: list):
+        super().__init__(parser, settings)
+
+    def get_rule_type(self):
+        return 'type'
+
+
+class AbstractSubjectRule(AbstractRule, abc.ABC):
+
+    def __init__(self, parser: Parser, settings: list):
+        super().__init__(parser, settings)
+
+    def get_rule_type(self):
+        return 'subject'
